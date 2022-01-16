0c729fda0a9dc73c1c832c4f34593046303c5df9
HWYS <htet.waiyan.swe@myancare.org>
2022-01-17 02:59:36 +0630

    Done checking

--- Hotels/Api/WeatherApiClient.swift
+++ Hotels/Api/WeatherApiClient.swift
@@ -8,16 +8,18 @@
 import Foundation
 
 class WeatherApiClient {
+    
     class func getCurrentWeatherByCoordinate(q: String, lat: Double, lon: Double, completion: @escaping (WeatherResponse?, Error?) ->  Void) {
         let headers = [
             "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
             "x-rapidapi-key": "ZX4BjoSRbjmshLRPbUXKVb8gOqcWp1QZ3HsjsnMJ1FQpV2rY9T"
         ]
         
-        do {
-            var request = try URLRequest(url: URL(string: "https://community-open-weather-map.p.rapidapi.com/weather?q=\(q.trimmingCharacters(in: .whitespacesAndNewlines))&lat=\(lat)&lon=\(lon)&lang=null&units=metric")! as URL,
+        if let url  = URL(string: "https://community-open-weather-map.p.rapidapi.com/weather?q=\(q.trimmingCharacters(in: .whitespacesAndNewlines))&lat=\(lat)&lon=\(lon)&lang=null&units=metric") {
+            var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy)
+            /*var request = try URLRequest(url: URL(string: )! as URL,
                                                     cachePolicy: .useProtocolCachePolicy,
-                                                timeoutInterval: 10.0)
+                                                timeoutInterval: 10.0)*/
             request.httpMethod = "GET"
             request.allHTTPHeaderFields = headers
 
@@ -45,11 +47,10 @@ class WeatherApiClient {
             })
 
             dataTask.resume()
-        }catch {
+        } else{
             DispatchQueue.main.async {
-                completion(nil, error)
+                completion(nil, nil)
             }
         }
-        
     }
 }
